84104ec8d606d48fcf656e60ce9f533a9bc634c5
Leo <31495285+hellomynameisleo@users.noreply.github.com>
2023-11-01 22:15:10 +0000

    Update yt-dlp.py

--- yt-dlp.py
+++ yt-dlp.py
@@ -8,6 +8,7 @@ init()
 # Set the path to your ytdlp installation directory
 ytdlp_path = os.path.dirname(os.path.abspath(__file__))
 downloadpath = os.path.expanduser("~/Downloads/")
+user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/113.0"
 
 # Check if ytdlp directory exists
 if not os.path.exists(ytdlp_path):
@@ -28,7 +29,8 @@ while True:
                            "2. Best audio\n-f bestaudio/best -v --extract-audio --audio-quality 0\n\n"  # Downloads the video with best audio then extracts the audio
                            "3. Best audio, download whole playlist\n-f bestaudio/best -v --extract-audio --audio-quality 0 --yes-playlist\n\n"  # Downloads the video with best audio then extracts the audio, downloads the whole playlist
                            "4. Best quality video and audio into single file\n-f bestvideo+bestaudio\n\n"  # Downloads the best quality video and audio streams separately and then merges them into a single file
-                           "Enter option number (1, 2, 3, or 4): ")
+                           "5. Download rule34hentai.net videos\n--cookies-from-browser firefox --user-agent user_agent\n\n"  # For Downloading from rule34hentai.net
+                           "Enter option number (1, 2, 3, 4, or 5): ")
 
     if options_choice == "1":
         download_options = ["-f", "ba", "-x", "--audio-format", "aac"]
@@ -42,6 +44,10 @@ while True:
     elif options_choice == "4":
         download_options = ["-f", "bestvideo+bestaudio"]
         break
+    elif options_choice == "5":
+        download_options = ["--cookies-from-browser", "firefox", "--user-agent", "user_agent"]
+        print ("Firefox user agent:", user_agent)
+        break
     else:
         print(Fore.YELLOW + options_choice, end=" ")
         print(Fore.RED + "is a Invalid option. Please select a valid option.")
@@ -49,6 +55,8 @@ while True:
 
 print(Fore.GREEN + "Selected download options:", download_options)
 print(Style.RESET_ALL)
+
+set_title_option = input("Do you want to automatically set the title using '%(title)s.%(ext)s'? (y/n): ")
 
 while True:
     # Prompt for YouTube URLs
@@ -66,6 +74,15 @@ while True:
 
         # Download video with specified options
         os.chdir(ytdlp_path)
-        subprocess.call(['yt-dlp.exe', youtube_url, '--output', '%(title)s.%(ext)s', '--paths', downloadpath] + download_options)
+
+        if set_title_option.lower() == "y":
+            # Automatically set the title using '%(title)s.%(ext)s'
+            subprocess.call(['yt-dlp.exe', youtube_url, '--output', '%(title)s.%(ext)s', '--paths', downloadpath] + download_options)
+        else:
+            # Manually set the title using the user's input
+            set_title = input("Input title of video: ")
+            output_filename = f"{set_title}.%(ext)s"
+            subprocess.call(['yt-dlp.exe', youtube_url, '--output', output_filename, '--paths', downloadpath] + download_options)
+
         print(f"Finished downloading '{youtube_url}' to '{downloadpath}'")
         print()
